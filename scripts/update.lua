64f501add37f611f2b71b28a4b701965ab15a7cf
ruki <waruqi@gmail.com>
2022-02-24 22:37:13 +0800

    update scripts

---
+++ scripts/update.lua
@@ -0,0 +1,13 @@
+import("core.base.option")
+import("core.base.json")
+import("net.http")
+
+function main()
+    local updateinfo = io.load(path.join(os.scriptdir(), "..", "update.txt"))
+    local version = updateinfo.version
+    local tag = "v" .. version
+    local assets = os.iorunv("gh", {"release", "view", tag, "--json", "assets", "--repo", "xmake-io/xmake"})
+    print(assets)
+    local assets_json = assert(json.decode(assets).assets, "assets not found!")
+    print(assets_json)
+end
