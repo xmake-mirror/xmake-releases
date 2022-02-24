684eeb544f5d4911c3116c5404e913082fdd9283
ruki <waruqi@gmail.com>
2022-02-24 23:36:49 +0800

    update script

--- scripts/update.lua
+++ scripts/update.lua
@@ -18,7 +18,8 @@ function main()
     for _, asset in ipairs(assets_json) do
         http.download(asset.url, path.join("assets", asset.name))
     end
-    print("preparing files ..")
+    -- publish to xmake-releases
+    print("publish to xmake-releases ..")
     os.cp("assets/xmake-v" .. version .. ".gz.run", ".")
     os.cp("assets/xmake-v" .. version .. ".xz.run", ".")
     os.cp("assets/xmake-v" .. version .. ".tar.gz", ".")
@@ -35,6 +36,7 @@ function main()
     os.exec("git push git@github.com:xmake-mirror/xmake-releases.git %s", version)
     os.exec("git push git@gitee.com:xmake-mirror/xmake-releases.git %s", version)
     os.exec("git push git@gitlab.com:xmake-mirror/xmake-releases.git %s", version)
+    -- publish to aur
     print("publish to aur ..")
     local sha256 = hash.sha256("xmake-v" .. version .. ".tar.gz")
     os.cd("..")
@@ -48,4 +50,15 @@ function main()
     os.exec("git add -A")
     os.exec("git commit -a -m \"update %s\"", version)
     os.exec("git push origin master")
+    -- publish to mingw-packages
+    os.cd("..")
+    os.exec("git clone https://github.com/waruqi/MINGW-packages.git")
+    version = "2.6.1"
+    os.cd("MINGW-packages")
+    io.gsub("mingw-w64-xmake/PKGBUILD", "pkgver=%d+%.%d+%.%d+", "pkgver=" .. version)
+    io.gsub("mingw-w64-xmake/PKGBUILD", "sha256sums=%('.-'%)", "sha256sums=('" .. sha256 .. "')")
+    os.exec("git diff")
+    os.exec("git add -A")
+    os.exec("git commit -a -m \"xmake: %s\"", version)
+    os.execv("gh", {"pr", "create", "--title", "xmake: " .. version, "--body", ""})
 end
