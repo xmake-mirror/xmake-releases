7632508fd3caf6458fa707b6ad1985e1f9f7f833
ruki <waruqi@gmail.com>
2022-02-24 22:47:18 +0800

    update ci and script

--- scripts/update.lua
+++ scripts/update.lua
@@ -5,8 +5,7 @@ import("net.http")
 function main()
     local updateinfo = io.load(path.join(os.scriptdir(), "..", "update.txt"))
     local version = updateinfo.version
-    local tag = "v" .. version
-    local assets = os.iorunv("gh", {"release", "view", tag, "--json", "assets", "--repo", "xmake-io/xmake"})
+    local assets = os.iorunv("gh", {"release", "view", "v" .. version, "--json", "assets", "--repo", "xmake-io/xmake"})
     local assets_json = assert(json.decode(assets).assets, "assets not found!")
     os.mkdir("assets")
     for _, asset in ipairs(assets_json) do
@@ -14,17 +13,17 @@ function main()
     end
     os.exec("git clone git@github.com:xmake-mirror/xmake-releases.git")
     os.cd("xmake-releases")
-    local ok = try {function() os.exec("git checkout %s", tag); return true end}
+    local ok = try {function() os.exec("git checkout %s", version); return true end}
     if not ok then
-        os.exec("git branch %s", tag)
-        os.exec("git checkout %s", tag)
+        os.exec("git branch %s", version)
+        os.exec("git checkout %s", version)
         os.rm("*")
     end
-    os.cp("../assets/xmake-" .. tag .. ".gz.run", ".")
-    os.cp("../assets/xmake-" .. tag .. ".xz.run", ".")
-    os.cp("../assets/xmake-" .. tag .. ".tar.gz", ".")
-    os.cp("../assets/xmake-" .. tag .. ".zip", ".")
-    os.exec("zip xmake-" .. tag .. ".win32.exe.zip ../assets/xmake-" .. tag .. ".win32.exe")
-    os.exec("zip xmake-" .. tag .. ".win64.exe.zip ../assets/xmake-" .. tag .. ".win64.exe")
+    os.cp("../assets/xmake-v" .. version .. ".gz.run", ".")
+    os.cp("../assets/xmake-v" .. version .. ".xz.run", ".")
+    os.cp("../assets/xmake-v" .. version .. ".tar.gz", ".")
+    os.cp("../assets/xmake-v" .. version .. ".zip", ".")
+    os.exec("zip xmake-v" .. version .. ".win32.exe.zip ../assets/xmake-v" .. version .. ".win32.exe")
+    os.exec("zip xmake-v" .. version .. ".win64.exe.zip ../assets/xmake-v" .. version .. ".win64.exe")
     os.exec("git status")
 end
