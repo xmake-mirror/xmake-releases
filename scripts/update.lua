200cf721b65a5e3d7650825dda8d42ad36f2b48f
ruki <waruqi@gmail.com>
2022-05-25 22:09:26 +0800

    Update update.lua

--- scripts/update.lua
+++ scripts/update.lua
@@ -20,6 +20,7 @@ function main()
     end
     -- publish to xmake-releases
     print("publish to xmake-releases ..")
+    --[[
     os.cp("assets/xmake-v" .. version .. ".gz.run", ".")
     os.cp("assets/xmake-v" .. version .. ".xz.run", ".")
     os.cp("assets/xmake-v" .. version .. ".tar.gz", ".")
@@ -36,6 +37,7 @@ function main()
     os.exec("git push git@github.com:xmake-mirror/xmake-releases.git %s", version)
     os.exec("git push git@gitee.com:xmake-mirror/xmake-releases.git %s", version)
     os.exec("git push git@gitlab.com:xmake-mirror/xmake-releases.git %s", version)
+    ]]
     -- publish to aur
     print("publish to aur ..")
     local sha256 = hash.sha256("xmake-v" .. version .. ".tar.gz")
